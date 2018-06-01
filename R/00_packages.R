6210165e42ca47e865ba6040a4862efac096a050
Carlos Munoz <dbi_cmunoz530@pedagogica.edu.co>
2018-05-31 22:47:45 -0500

    Adding packages

--- R/00_packages.R
+++ R/00_packages.R
@@ -1,7 +1,10 @@
+# Thesis packages
+# Operational
 thesis_packages <- c("raster", "data.table", "dplyr", "spocc", "rgbif",
 "rlist", "sp", "stringr", "plyr", "rgeos", "scrubr", "dismo", "vegan",
 "rgdal", "sqldf", "maps", "testthat", "rlist", "ENMeval", "rowr",
-"doSNOW", "foreach", "gridExtra", "plotrix", "ggplot2", "colorRamps")
+"doSNOW", "foreach", "gridExtra", "plotrix", "ggplot2", "colorRamps",
+"scatterplot3d", "SpatialPack", "FSA")
 
 missing_pkgs <- thesis_packages[which(!thesis_packages %in% installed.packages())]
 if(length(missing_pkgs)) install.packages(missing_pkgs)
@@ -9,6 +12,7 @@ if(length(missing_pkgs)) install.packages(missing_pkgs)
 if (!require('devtools')) install.packages('devtools')
 devtools::install_github('luismurao/ntbox')
 
+# Styler
 thesis_stylishcode <- c("roxygen2", "styler")
 missing_stylishcode <- thesis_stylishcode[which(!thesis_stylishcode %in% installed.packages())]
 if(length(missing_stylishcode)) install.packages(missing_stylishcode)
